4560d3bd24421e6120141602f5c0b1fa81ca5334
E2Kerem01 <62817020+E2Kerem01@users.noreply.github.com>
2021-12-17 14:26:09 +0300

    Update empty.py

--- empty.py
+++ empty.py
@@ -4,3 +4,24 @@ def kerem():
     
         print("    i        ")
         
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(form.password.data)
+
+        cursor = mysql.connection.cursor()
+
+        sorgu = "Insert into users(name,email,username,password) VALUES(%s,%s,%s,%s)"
+
+        cursor.execute(sorgu,(name,email,username,password))
+        mysql.connection.commit()
+
+        cursor.close()
+        flash("Başarıyla Kayıt Oldunuz...","success")
+        return redirect(url_for("login"))
+    else:
+        return render_template("register.html",form = form)        
